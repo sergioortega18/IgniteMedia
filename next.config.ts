a4a4d053c73d917b28ae9e32c79ebdbf13334e0f
sergioortega18 <sergio_ortega70232@elpoli.edu.co>
2025-02-04 20:42:14 -0500

    config22

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
-// next.config.js
-module.exports = {
-  TrailingSlash: true,
-  generateStaticParams: async function (
-    _defaultPathMap: any,
-    { dev, dir, outDir, distDir, buildId }: any
-  ) {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      // Agrega aquí todas las rutas de tu aplicación
-    }
-  },
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
 }
+ 
+module.exports = nextConfig
