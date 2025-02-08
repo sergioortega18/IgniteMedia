8a66bd2cfd1eec453e984a129ca3cd0a06021eb9
sergioortega18 <sergio_ortega70232@elpoli.edu.co>
2025-02-07 20:22:53 -0500

    final

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,156 +1,123 @@
 "use client";
 
-import { AnimatePresence, motion, useInView, useScroll, useTransform } from 'framer-motion';
-import { FC, useCallback, useEffect, useRef, useState } from 'react';
+import { FC, useEffect } from 'react';
 import useTextRevealAnimation from '@/hooks/useTextRevealAnimation';
-import image1 from "@/assets/images/hero-image.jpg";
-import image2 from "@/assets/images/hero-image.jpg";
-import image3 from "@/assets/images/hero-image.jpg";
-import Test from '@/components/Test';
+import image1 from "@/assets/images/project-1.jpg";
+import image2 from "@/assets/images/project-2.jpg";
+import image3 from "@/assets/images/project-3.jpg";
+import image4 from "@/assets/images/project-4.jpg";
+import image5 from "@/assets/images/project-5.jpg";
+import Image from "next/image";
 
-const abouts = [
+const projects = [
     {
-        name: "Sarah Chen",
-        company: "Pixel Perfect",
-        role: "Head of Design",
-        quote: "Alex's expertise in both technical development and design created a beautiful, high-performing website.",
+        name: "Artisan Brew Co.",
         image: image1,
-        imagePositionY: 0.2,
     },
     {
-        name: "Marcus Rodriguez",
-        company: "Craft Coffee Co.",
-        role: "Founder",
-        quote: "Alex transformed our boutique coffee brand with a website that perfectly balances aesthetics and functionality.",
+        name: "Wavelength Studios",
         image: image2,
-        imagePositionY: 0.2,
     },
     {
-        name: "Emily Watson",
-        company: "Studio Minimal",
-        role: "Creative Director",
-        quote: "The collaborative process was amazing. Alex brought lots of fresh perspectives and innovative solutions.",
+        name: "Nova Fitness",
         image: image3,
-        imagePositionY: 0.2,
+    },
+    {
+        name: "Urban Plates",
+        image: image4,
+    },
+    {
+        name: "Bloom Botanicals",
+        image: image5,
     },
 ];
 
-const About: FC = () => {
-
-    const titleRef = useRef(null);
-
-    const { scrollYProgress } = useScroll({
-        target: titleRef,
-        offset: ['start end', 'end start'],
-    });
-
-    const transformTop = useTransform(scrollYProgress, [0, 1], ["-10%", "15%"]);
-    const transformBottom = useTransform(scrollYProgress, [0, 1], ["10%", "-15%"]);
-    
-    const sectionRef = useRef(null);
+const Intro: FC = () => {
     const { scope, entranceAnimation } = useTextRevealAnimation();
-    const inView = useInView(scope, {
-        once: true,
-    });
-
-    const [hasAnimated, setHasAnimated] = useState(false);
+    const inView = useInView(scope);
 
     useEffect(() => {
-        if (inView && !hasAnimated) {
+        if (inView) {
             entranceAnimation();
-            setHasAnimated(true);
         }
-    }, [inView, entranceAnimation, hasAnimated]);
-
-    const [testimonialIndex, setTestimonialIndex] = useState(0);
-
-    const handleClickPrev = useCallback(() => {
-        setTestimonialIndex((prevIndex) => (prevIndex - 1 + abouts.length) % abouts.length);
-    }, []);
-
-    const handleClickNext = useCallback(() => {
-        setTestimonialIndex((prevIndex) => (prevIndex + 1) % abouts.length);
-    }, []);
+    }, [inView, entranceAnimation]);
 
     return (
         <section
-            className="relative"
-            id="about"
-            ref={sectionRef}
+            className="section relative text-[#E61D40] text-[16px] md:text-[16px] lg:text-[32px] font-sans font-light leading-[1.2] uppercase"
+            id="intro"
         >
-            <div className='px-4 md:px-6 pt-12 md:pt-56 lg:mx-16 pb-12'>
-                <div ref={scope}>
-                    <h2
-                        className="text-[32px] md:text-[75px] lg:text-[96px] leading-[1.2] lg:w-[80%] font-serif"
-                    >
-                        We <span className='italic'>are a </span> Tasted-led
-                        <br />
-                        Growth Studio <span className="italic"> from</span>
-                        <br />
-                        Medellin
-                        <br />
-                    </h2>
-
-                    <p className='font-medium font-sans text-[12px] md:text-[22px] lg:text-[28px] pt-6 lg:pt-9  md:leading-[1.3] lg:w-[80%]'>
-                        {/* <span className='font-semibold font-sans'>IGNITE MEDIA</span> is a creative growth studio led by taste. We specialize in holistic growth solutions for brands across tech, CPG, hospitality, and luxury brands. We specialize in paid social advertising, Google advertising, content creation, and event strategy. Our mission at IGNITE MEDIA is to help beloved brands grow efficiently, sustainably, and profitably. */}
+            <div className='px-4'>
+                <div
+                    className='text-[#E61D40] text-[16px] md:text-[24px] lg:text-[32px] font-sans font-light leading-[1.2] uppercase'
+                    ref={scope}
+                >
+                    <p className="md:px-36 pb-8">
+                        We Create Comprehensive and Customized Solutions to Help Our Clients Achieve Their
+                        Goals in the Digital World.
+                    </p>
+                    <p className='md:mb-20 pb-8 md:pl-48 md:pr-20'>
+                        We specialize in holistic growth solutions for brands across tech, CPG, hospitality, and luxury brands.
+                        We specialize in content creation, social media advertising, Google advertising, and event strategy.
+                        Our mission at IGNITE MEDIA is to help beloved brands grow efficiently, sustainably, and profitably.
                     </p>
                 </div>
 
-                <div className="relative">
-                    <div className="mt-8 md:mt-14">
-                        <AnimatePresence mode="wait" initial={false}>
-                            {abouts.map((about, index) => (
-                                index === testimonialIndex && (
-                                    <Test
-                                        key={about.name}
-                                        name={about.name}
-                                        company={about.company}
-                                        role={about.role}
-                                        quote={about.quote}
-                                        image={about.image}
-                                        imagePositionY={about.imagePositionY}
-                                    />
-                                )
-                            ))}
-                        </AnimatePresence>
-                    </div>
+                <div className="mt-6 md:mt-16">
+                    {projects.map(({ name, image }) => (
+                        <a
+                            href='#'
+                            key={name}
+                            className="border-t last:border-b border-stone-400 border-dotted md:py-8 flex flex-col relative group/project"
+                            aria-label={`View ${name} project`}
+                        >
+                            <div className="absolute bottom-0 left-0 w-full h-0 group-hover/project:h-full transition-all duration-700 bg-stone-300"></div>
 
-                    <div className="flex gap-4 mt-6 lg:mt-10">
-                        <button
-                            className="border border-stone-400 size-11 inline-flex items-center justify-center rounded-full"
-                            onClick={handleClickPrev}
-                            aria-label="Previous testimonial"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-                            </svg>
-                        </button>
-                        <button
-                            className="border border-stone-400 size-11 inline-flex items-center justify-center rounded-full"
-                            onClick={handleClickNext}
-                            aria-label="Next testimonial"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
-                            </svg>
-                        </button>
-                    </div>
+                            <div className="relative">
+                                <div className="my-4 md:mt-0 flex justify-between items-center md:grid md:[grid-template-columns:1fr_300px_max-content] md:gap-8">
+                                    <div className="md:ml-6 lg:group-hover/project:pl-12 transition-all duration-700">
+                                        <h3 className="text-[38px] font-serif md:text-[7vw]">{name}</h3>
+                                    </div>
+
+                                    <div className="relative">
+                                        <div className="absolute aspect-video w-full top-1/2 -translate-y-1/2 opacity-0 scale-90 group-hover/project:opacity-100 group-hover/project:scale-100 lg:group-hover/project:scale-120 transition-all duration-500 z-10">
+                                            <Image
+                                                src={image}
+                                                alt={`${name} image`}
+                                                className="size-full object-cover"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="lg:group-hover/project:px-8 transition-all duration-500">
+                                        <div className="size-6 overflow-hidden">
+                                            <div className="h-6 w-12 flex group-hover/project:-translate-x-1/2 transition-transform duration-300">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                                                </svg>
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="w-full md:hidden">
+                                    <div className="relative w-full pt-[177.778%]"> {/* 16:9 aspect ratio */}
+                                        <Image
+                                            src={image}
+                                            alt={`${name} image`}
+                                            fill
+                                            className="absolute top-0 left-0 w-full h-full object-cover"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    ))}
                 </div>
-            </div>
-
-            
-            <div className="flex justify-center py-8 md:py-20">
-                    <h2 className="text-5xl md:text-8xl lg:text-[234px] flex flex-col overflow-hidden tracking-tighter" ref={titleRef}>
-                        <motion.span className="whitespace-nowrap font-serif" style={{ x: transformTop }}>
-                        Get <span className="italic">in </span> Touch Get <span className="italic">in </span> Touch   
-                        </motion.span>
-                        <motion.span className="whitespace-nowrap self-end font-serif" style={{ x: transformBottom }}>
-                        Get <span className="italic text-[#E61D40]">in</span> Touch Get <span className="italic text-[#E61D40]">in</span> Touch
-                        </motion.span>
-                    </h2>
             </div>
         </section>
     );
 };
 
-export default About;
+export default Intro;
